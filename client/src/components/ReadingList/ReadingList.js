cfc30c86aecc892913cecdb0d2c15eb994f8d24a
Eshed Margalit <eshed.margalit@gmail.com>
2019-09-16 21:29:24 -0700

    hooks up start review button in reading list

--- client/src/components/ReadingList/ReadingList.js
+++ client/src/components/ReadingList/ReadingList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { start_review } from "../../actions/index";
 import {
   sortableContainer,
   sortableElement,
@@ -8,11 +9,10 @@ import {
 import { Button, Icon, List, PageHeader } from "antd";
 import moment from "moment";
 import Infinite from "react-infinite";
-import { shortenAuthors, shortenString } from "../utils";
+import { shortenAuthors } from "../utils";
 
 const LIST_HEIGHT = 330;
 const ITEM_HEIGHT = 100;
-const MAX_TITLE_LENGTH = 80; // characters
 
 const DragHandle = sortableHandle(() => (
   <Icon
@@ -21,42 +21,35 @@ const DragHandle = sortableHandle(() => (
   />
 ));
 
-const SortableItem = sortableElement(({ height, value, sortIndex }) => (
-  <List.Item style={{ height }}>
-    <DragHandle />
-    <div
-      style={{
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between"
-      }}
-    >
-      <div>
-        <List.Item.Meta
-          title={`#${sortIndex + 1}: ${shortenString(
-            value.metadata.title,
-            MAX_TITLE_LENGTH
-          )}`}
-          description={`${shortenAuthors(value.metadata.authors)}, ${moment(
-            value.metadata.date,
-            "YYYY-MM"
-          ).format("YYYY")}`}
-        />
+const SortableItem = sortableElement(
+  ({ height, value, sortIndex, clickHandler }) => (
+    <List.Item style={{ height }}>
+      <DragHandle />
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between"
+        }}
+      >
+        <div>
+          <List.Item.Meta
+            title={`#${sortIndex + 1}: ${value.metadata.title}`}
+            description={`${shortenAuthors(value.metadata.authors)}, ${moment(
+              value.metadata.date,
+              "YYYY-MM"
+            ).format("YYYY")}`}
+          />
+        </div>
+        <div>
+          <Button onClick={() => clickHandler(value)} icon="form" />
+        </div>
       </div>
-      <div>
-        <Button
-          onClick={() => {
-            console.log(value);
-          }}
-        >
-          Start Review <Icon type="form" />
-        </Button>
-      </div>
-    </div>
-  </List.Item>
-));
+    </List.Item>
+  )
+);
 
-const SortableInfiniteList = sortableContainer(({ items }) => {
+const SortableInfiniteList = sortableContainer(({ items, clickHandler }) => {
   return (
     <List bordered={true}>
       <Infinite containerHeight={LIST_HEIGHT} elementHeight={ITEM_HEIGHT}>
@@ -67,6 +60,7 @@ const SortableInfiniteList = sortableContainer(({ items }) => {
             sortIndex={index}
             value={value}
             height={ITEM_HEIGHT}
+            clickHandler={clickHandler}
           />
         ))}
       </Infinite>
@@ -75,6 +69,10 @@ const SortableInfiniteList = sortableContainer(({ items }) => {
 });
 
 class ReadingList extends Component {
+  handleClick(value) {
+    this.props.dispatch(start_review(value));
+  }
+
   render() {
     return (
       <div>
@@ -83,6 +81,7 @@ class ReadingList extends Component {
         <SortableInfiniteList
           onSortEnd={this.props.onSortEnd}
           items={this.props.items}
+          clickHandler={this.handleClick.bind(this)}
           useDragHandle
         />
       </div>
