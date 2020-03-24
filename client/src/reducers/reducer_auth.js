3f73dced7de3e3151fafa17dbd7496ff239137af
Eshed Margalit <eshed.margalit@gmail.com>
2020-03-23 21:42:45 -0700

    remove un-needed call to console.log

--- client/src/reducers/reducer_auth.js
+++ client/src/reducers/reducer_auth.js
@@ -3,7 +3,6 @@ import { FETCH_USER } from '../actions/index';
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
-      console.log(action.payload);
       return action.payload || false; // "" ==> falsy
     default:
       return state;
