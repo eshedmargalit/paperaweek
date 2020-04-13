4a2743e53fab360b688ccc80583343f591dd23b6
Eshed Margalit <eshed.margalit@gmail.com>
2020-04-12 18:46:52 -0700

    removes current points text in modal

--- client/src/components/PointsModal/PointsModal.js
+++ client/src/components/PointsModal/PointsModal.js
@@ -11,17 +11,13 @@ class Login extends Component {
   componentDidMount() {}
 
   render() {
-    let { increment, reason, points, displayModal } = this.props.points;
+    let { increment, reason, displayModal } = this.props.points;
     const incrementString = increment === 1 ? 'point' : 'points';
-    const currentPointsString = points === 1 ? 'point' : 'points';
     return (
       <Modal title="Congratulations! 🎉" visible={displayModal} onOk={this.closeModal} onCancel={this.closeModal}>
         <h3>
           You just earned {increment} {incrementString}!
         </h3>
-        <p>
-          That brings you up to {points} {currentPointsString}, nice!{' '}
-        </p>
         <div dangerouslySetInnerHTML={{ __html: reason }}></div>
       </Modal>
     );
