4142cded03d3df7f3d1f2a02ac12212a8e83b489
Arad Margalit <aradmar@gmail.com>
2020-05-04 21:58:38 -0700

    Fixes wonky button width

--- client/src/components/ReviewWizard/ReviewForm.js
+++ client/src/components/ReviewWizard/ReviewForm.js
@@ -178,7 +178,7 @@ class ReviewForm extends Component {
       <Form layout="vertical" onSubmit={this.validateFields}>
         {renderedFields}
         <Form.Item {...formItemLayoutWithoutLabel}>
-          <Button type="primary" htmlType="submit" style={{ width: '30%' }}>
+          <Button type="primary" htmlType="submit" style={{ width: '200px' }}>
             Next: Preview and Submit
           </Button>
         </Form.Item>
