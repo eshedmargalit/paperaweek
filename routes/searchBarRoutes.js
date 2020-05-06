526df68dacf61048c22fe5adb161412c01ace0c1
Arad Margalit <aradmargalit@users.noreply.github.com>
2020-05-05 17:55:16 -0700

    Removes complete=1 flag (#43)

--- routes/searchBarRoutes.js
+++ routes/searchBarRoutes.js
@@ -6,7 +6,7 @@ const endpoint = "https://api.labs.cognitive.microsoft.com/academic/v1.0";
 module.exports = app => {
   app.get("/api/searchBar/interpret/:query", async (req, res) => {
     const query = req.params.query;
-    const interpret_query = `${endpoint}/interpret?query=${query}&complete=1&count=1&subscription-key=${process.env.REACT_APP_MSCOG_KEY1}`;
+    const interpret_query = `${endpoint}/interpret?query=${query}&count=1&subscription-key=${process.env.REACT_APP_MSCOG_KEY1}`;
     const response = await axios(interpret_query);
     res.send(JSON.stringify(response.data));
   });
