25ede7f1a9ab14c7fc82717e78b9e9b7ca6de221
Eshed Margalit <eshed.margalit@gmail.com>
2020-08-09 12:54:18 -0400

    minor; adds alt text to login buttons and removes unused imports

--- client/src/components/MenuBar/MenuBar-view.js
+++ client/src/components/MenuBar/MenuBar-view.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Badge, Button } from 'antd';
-import { GoogleOutlined, UserOutlined } from '@ant-design/icons';
+import { UserOutlined } from '@ant-design/icons';
 import GoogleButton from '../GoogleButton';
 
 import './MenuBar.scss';
