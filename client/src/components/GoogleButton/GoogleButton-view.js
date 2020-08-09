f83b575b1dbe5df311b33c3b8e4c560c3da74b52
Eshed Margalit <eshed.margalit@gmail.com>
2020-08-09 12:49:35 -0400

    adds Google-approved sign in button, with state management to set button appearance and different colors for menu and login

---
+++ client/src/components/GoogleButton/GoogleButton-view.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function GoogleButtonView({ colorMode, height }) {
+  const [buttonMode, setButtonMode] = useState('normal');
+
+  const buttons = {
+    normal: require(`../../img/btn_google_signin_${colorMode}_normal_web.png`),
+    focus: require(`../../img/btn_google_signin_${colorMode}_focus_web.png`),
+    pressed: require(`../../img/btn_google_signin_${colorMode}_pressed_web.png`),
+  };
+
+  const src = buttons[buttonMode];
+
+  const button = (
+    <a
+      onFocus={() => setButtonMode('focus')}
+      onMouseEnter={() => setButtonMode('focus')}
+      onMouseLeave={() => setButtonMode('normal')}
+      onClick={() => setButtonMode('pressed')}
+      href="/auth/google"
+      size={'large'}
+    >
+      <img src={src} height={'100%'} />
+    </a>
+  );
+
+  return button;
+}
+
+export default GoogleButtonView;
