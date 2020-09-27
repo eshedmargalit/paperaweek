d67d890b841b40e73091fecc0158b5dd11e4a1f3
Eshed Margalit <eshed.margalit@gmail.com>
2020-09-27 09:46:09 -0400

    paper search bar functional-component-ified

--- client/src/components/PaperSearchBar/PaperSearchBar-redux.js
+++ client/src/components/PaperSearchBar/PaperSearchBar-redux.js
@@ -1,50 +1,42 @@
-import React, { Component } from 'react';
 import axios from 'axios';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { setReview, updateReadingList } from '../../actions';
 import PaperSearchBarContainer from './PaperSearchBar-container';
 
-class PaperSearchBarRedux extends Component {
-  setBlankReview = () => {
-    this.props.dispatch(setReview(null, null));
+export default function PaperSearchBarRedux() {
+  const dispatch = useDispatch();
+  let readingList = useSelector(state => state.readingList);
+
+  const setBlankReview = () => {
+    dispatch(setReview(null, null));
   };
 
-  updateReadingList = async newReadingList => {
-    this.props.dispatch(updateReadingList(newReadingList));
+  const updateReadingListFunc = async newReadingList => {
+    dispatch(updateReadingList(newReadingList));
 
     const res = await axios.put('api/readingList', newReadingList);
-    this.props.dispatch(updateReadingList(res.data));
+    dispatch(updateReadingList(res.data));
   };
 
-  handleClickResult = ({ paper }) => {
-    let { readingList } = this.props;
-    let newReadingList = readingList.concat(paper);
-    this.updateReadingList(newReadingList);
+  const handleClickResult = ({ paper }) => {
+    const newReadingList = readingList.concat(paper);
+    updateReadingListFunc(newReadingList);
   };
 
-  handleClickResultButton = ({ paper }) => {
-    let reviewContent = {
+  const handleClickResultButton = ({ paper }) => {
+    const reviewContent = {
       paper: paper,
       review: null,
     };
-    this.props.dispatch(setReview(null, reviewContent));
+    dispatch(setReview(null, reviewContent));
   };
 
-  render() {
-    return (
-      <PaperSearchBarContainer
-        setBlankReview={this.setBlankReview.bind(this)}
-        handleClickResult={this.handleClickResult.bind(this)}
-        handleClickResultButton={this.handleClickResultButton.bind(this)}
-      />
-    );
-  }
+  return (
+    <PaperSearchBarContainer
+      setBlankReview={setBlankReview}
+      handleClickResult={handleClickResult}
+      handleClickResultButton={handleClickResultButton}
+    />
+  );
 }
-
-const mapStateToProps = ({ readingList }) => {
-  return {
-    readingList,
-  };
-};
-
-export default connect(mapStateToProps, null)(PaperSearchBarRedux);
