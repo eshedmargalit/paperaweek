02f006565189a03b37a53994b76e5bf5959e3943
Eshed Margalit <eshed.margalit@gmail.com>
2020-09-27 10:11:35 -0400

    Preferences components all functional

--- client/src/components/Preferences/Preferences-redux.js
+++ client/src/components/Preferences/Preferences-redux.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import PreferencesContainer from './Preferences-container';
 import axios from 'axios';
 
-function PreferencesRedux({ auth, user, onChange }) {
+export default function PreferencesRedux({ onChange }) {
   const saveResults = async values => {
     await axios.put('/api/user', values);
     onChange();
   };
+  const auth = useSelector(state => state.auth);
+  const user = useSelector(state => state.user);
 
   let { publicProfile } = user;
   const initialValues = { publicProfile };
 
   return <PreferencesContainer auth={auth} initialValues={initialValues} saveResults={saveResults} />;
 }
-
-const mapStateToProps = ({ user, auth }) => {
-  return { user, auth };
-};
-
-export default connect(mapStateToProps, null)(PreferencesRedux);
