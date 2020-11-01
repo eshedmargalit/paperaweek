087ad9b80da7d5cd8ad8a977abd8923d2b821a41
Eshed Margalit <eshed.margalit@gmail.com>
2020-11-01 10:13:16 -0500

    moves labels above fields

--- client/src/components/ReviewWizard/utils.js
+++ client/src/components/ReviewWizard/utils.js
@@ -112,19 +112,19 @@ export function notEmpty(x) {
 
 export const formItemLayout = {
   labelCol: {
-    xs: { span: 4 },
-    sm: { span: 6 },
+    xs: { span: 24 },
+    sm: { span: 24 },
   },
   wrapperCol: {
     xs: { span: 24 },
-    sm: { span: 18 },
+    sm: { span: 24 },
   },
 };
 
 export const formItemLayoutWithoutLabel = {
   wrapperCol: {
     xs: { span: 24, offset: 0 },
-    sm: { span: 18, offset: 6 },
+    sm: { span: 24, offset: 0 },
   },
 };
 
