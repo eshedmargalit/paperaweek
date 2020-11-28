06691877ed4db1bc5071e701e41b2bfd3633ff94
Arad Margalit <aradmar@gmail.com>
2020-11-28 12:03:57 -0800

    ESLint instaled with styleguide

---
+++ server/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb/base'],
+};
---
+++ server/config/keys.ts
@@ -0,0 +1,4 @@
+export const googleClientID = process.env.GOOGLE_CLIENT_ID;
+export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+export const mongoURI = process.env.MONGO_URI;
+export const cookieKey = process.env.COOKIE_KEY;
