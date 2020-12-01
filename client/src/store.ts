67841cc2c7509db21bf6af20ac8f4bccece90236
Arad Margalit <aradmar@gmail.com>
2020-11-30 19:07:15 -0800

    first working TS

--- client/src/store.ts
+++ client/src/store.ts
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
 const composeEnhancers =
+  // @ts-ignore
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    ? // @ts-ignore
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         trace: true,
       })
     : compose;
