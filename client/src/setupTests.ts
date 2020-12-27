31752e086d2e68745e6289861e8eae268fdf2ed9
Arad Margalit <aradmargalit@users.noreply.github.com>
2020-12-27 13:14:11 -0800

    Adds MSW and example usage (#72)

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,5 +1,7 @@
 // This is a known limitation of antd, we need to mock matchMedia since JSDOM doesn't implement it
 // https://github.com/ant-design/ant-design/issues/21096
+import { server } from './mocks/server';
+
 global.matchMedia =
   global.matchMedia ||
   (() => ({
@@ -9,3 +11,10 @@ global.matchMedia =
   }));
 
 export {};
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
