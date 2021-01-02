1ad4bf1978759a381c0e8c2e2c7dc508c33f0a45
Arad Margalit <aradmargalit@users.noreply.github.com>
2021-01-02 10:02:55 -0800

    Finish <Home /> tests (#77)

--- client/src/components/Home/Home.test.tsx
+++ client/src/components/Home/Home.test.tsx
@@ -4,16 +4,39 @@ import { screen } from '@testing-library/react';
 import Home from '.';
 import { blankUser } from '../../templates';
 import { getBlankInitialState, renderWithRouterRedux } from '../../testUtils/reduxRender';
+import { RootState } from '../../reducers';
+
+// Helper to quickly render the component with the right Redux initialState
+const renderHome = (initialState?: RootState) => renderWithRouterRedux(<Home />, { initialState });
 
 describe('<Home />', () => {
-  it('renders without crashing', () => {
-    renderWithRouterRedux(<Home />, {
-      initialState: { ...getBlankInitialState(), auth: { ...blankUser, displayName: 'Jim Henderson' } },
+  describe('when nobody is logged in', () => {
+    it('redirects when no user is present', () => {
+      renderWithRouterRedux(<Home />, { redirectTo: '/' });
+      expect(screen.getByText(/Redirected to a new page./)).toBeDefined();
     });
   });
 
-  it('redirects when no user is present', () => {
-    renderWithRouterRedux(<Home />, { redirectTo: '/' });
-    expect(screen.getByText(/Redirected to a new page./)).toBeDefined();
+  describe('with a user logged in', () => {
+    const initialState: RootState = { ...getBlankInitialState(), auth: { ...blankUser, displayName: 'Jim Henderson' } };
+
+    it('renders without crashing', () => {
+      renderHome(initialState);
+    });
+
+    it('has a search bar', () => {
+      renderHome(initialState);
+      expect(screen.getAllByPlaceholderText(/search by DOI/)).toBeDefined();
+    });
+
+    it('has a reading list', () => {
+      renderHome(initialState);
+      expect(screen.getByText(/Reading List/)).toBeDefined();
+    });
+
+    it('has review list', () => {
+      renderHome(initialState);
+      expect(screen.getByText(/Read Your Reviews/)).toBeDefined();
+    });
   });
 });
