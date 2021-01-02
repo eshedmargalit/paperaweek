799f5c4d8f1a6b88bcdfe61ee383f31facdf021f
Arad Margalit <aradmar@gmail.com>
2021-01-01 16:05:07 -0800

    Example redux unit test

--- client/src/components/Home/Home.test.tsx
+++ client/src/components/Home/Home.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+
 import Home from '.';
-import { renderWithRedux } from '../../testUtils/reduxRender';
+import { blankUser } from '../../templates';
+import { getBlankInitialState, renderWithRouterRedux } from '../../testUtils/reduxRender';
 
 describe('<Home />', () => {
   it('renders without crashing', () => {
-    renderWithRedux(<Home />);
+    renderWithRouterRedux(<Home />, {
+      initialState: { ...getBlankInitialState(), auth: { ...blankUser, displayName: 'Jim Henderson' } },
+    });
+  });
+
+  it('redirects when no user is present', () => {
+    renderWithRouterRedux(<Home />, { redirectTo: '/' });
+    expect(screen.getByText(/Redirected to a new page./)).toBeDefined();
   });
 });
--- client/src/testUtils/reduxRender.tsx
+++ client/src/testUtils/reduxRender.tsx
@@ -2,32 +2,47 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render, RenderResult } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { applyMiddleware, createStore, Store } from 'redux';
 import thunk from 'redux-thunk';
+import { create } from 'lodash';
 import rootReducer, { RootState } from '../reducers';
 
-interface RenderWithReduxOptions {
+interface RenderWithRouterReduxOptions {
+  redirectTo?: string;
   initialState?: RootState;
   store?: Store;
 }
 
-interface RenderWithReduxResult extends RenderResult {
+interface RenderWithRouterReduxResult extends RenderResult {
   store: Store;
 }
 /**
  * Render the component wrapped in a mock router and a Redux provider
  */
-export function renderWithRedux(
+export function renderWithRouterRedux(
   ui: React.ReactElement,
-  { initialState, store = createStore(rootReducer, initialState, applyMiddleware(thunk)) }: RenderWithReduxOptions = {}
-): RenderWithReduxResult {
+  {
+    redirectTo,
+    initialState,
+    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
+  }: RenderWithRouterReduxOptions = {}
+): RenderWithRouterReduxResult {
   return {
     ...render(
-      <MemoryRouter>
-        <Provider store={store}>{ui}</Provider>
+      <MemoryRouter initialEntries={['/starting']}>
+        <Route path="/starting">
+          <Provider store={store}>{ui}</Provider>
+        </Route>
+        <Route path={redirectTo}>Redirected to a new page.</Route>
       </MemoryRouter>
     ),
     store,
   };
+}
+
+// Exported to help tests quickly get a blank initialState to work with
+export function getBlankInitialState(): RootState {
+  const store = createStore(rootReducer, applyMiddleware(thunk));
+  return store.getState();
 }
