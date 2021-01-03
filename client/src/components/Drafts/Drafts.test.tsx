ddf4ab3fc8828c25eed04d2ceb24dcfa7078d641
Arad Margalit <aradmar@gmail.com>
2021-01-03 14:12:02 -0800

    DraftsRedux test

---
+++ client/src/components/Drafts/Drafts.test.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-console */
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { RootState } from '../../reducers';
+import { blankNotes, blankReview } from '../../templates';
+import { getBlankInitialState, renderWithRouterRedux } from '../../testUtils/reduxRender';
+import DraftsRedux from './Drafts-redux';
+
+const mockGoBack = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...(jest.requireActual('react-router-dom') as any),
+  useHistory: () => ({
+    goBack: mockGoBack,
+  }),
+}));
+
+const initialStateWithDrafts: RootState = {
+  ...getBlankInitialState(),
+  drafts: [{ ...blankReview, _id: 'id', notes: { ...blankNotes, tldr: 'it was nice' } }],
+};
+
+describe('<Drafts />', () => {
+  describe('on back', () => {
+    it('goes back to the previous page', () => {
+      renderWithRouterRedux(<DraftsRedux />);
+      const backButton = screen.getByLabelText('Back');
+      userEvent.click(backButton);
+      expect(mockGoBack).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('drafts table', () => {
+    // This is silly, but the react-katex library throws an ugly warning: https://github.com/talyssonoc/react-katex/issues/59
+    // so we'll just suppress it :)
+    const originalConsoleWarn = console.warn;
+    beforeAll(() => (console.warn = jest.fn()));
+    afterAll(() => (console.warn = originalConsoleWarn));
+
+    it('renders without crashing', () => {
+      renderWithRouterRedux(<DraftsRedux />);
+    });
+
+    it('renders the draft information in the table', () => {
+      renderWithRouterRedux(<DraftsRedux />, { initialState: initialStateWithDrafts });
+      expect(screen.getByText(/it was nice/)).toBeDefined();
+    });
+
+    it('displays more options and actions when user clicks on a draft', () => {
+      renderWithRouterRedux(<DraftsRedux />, { initialState: initialStateWithDrafts });
+      const draftRow = screen.getByText(/it was nice/);
+      userEvent.click(draftRow);
+      expect(screen.getByText(/Delete this Draft/)).toBeDefined();
+    });
+  });
+});
