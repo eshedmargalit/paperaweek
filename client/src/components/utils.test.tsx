a39dc1068eb302a8a904a76e25b7bd5282b00e9e
Arad Margalit <aradmar@gmail.com>
2021-01-03 13:08:08 -0800

    self review feedback

--- client/src/components/utils.test.tsx
+++ client/src/components/utils.test.tsx
@@ -2,7 +2,6 @@
 /* eslint-disable no-return-assign */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { before } from 'lodash';
 import {
   getTagColor,
   HSLString,
