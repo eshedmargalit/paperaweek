a60749b4572bb901e7ffb275381a835ecead48c4
Arad Margalit <aradmar@gmail.com>
2021-01-03 22:30:07 -0800

    more paper search tests

--- client/src/components/PaperSearchBar/PaperSearchBar.test.tsx
+++ client/src/components/PaperSearchBar/PaperSearchBar.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
 import PaperSearchBar from '.';
 import { renderWithRouterRedux } from '../../testUtils/reduxRender';
+import { server } from '../../mocks/server';
 
 describe('<PaperSearchBar />', () => {
   describe('header', () => {
@@ -51,6 +53,36 @@ describe('<PaperSearchBar />', () => {
         // This mock data we're expecting is defined in handlers.ts
         await waitFor(() => expect(screen.getByText('Test DOI Title')).toBeDefined());
       });
+    });
+  });
+
+  describe('manual entry buttons', () => {
+    it('the first one redirects to the form page', () => {
+      renderWithRouterRedux(<PaperSearchBar />, { redirectTo: '/form' });
+      userEvent.click(screen.getAllByText(/Create Manual Entry/)[0]);
+      expect(screen.getByText(/Redirected to a new page/)).toBeDefined();
+    });
+
+    it('the second one redirects to the form page', () => {
+      renderWithRouterRedux(<PaperSearchBar />, { redirectTo: '/form' });
+      userEvent.click(screen.getAllByText(/Create Manual Entry/)[1]);
+      expect(screen.getByText(/Redirected to a new page/)).toBeDefined();
+    });
+  });
+
+  describe('results', () => {
+    it('shows not found if none are found', async () => {
+      // For this test, we want no papers to be returned, so override the mock server handler
+      server.use(rest.get('/api/searchBar/interpret/notFound', (_req, res, ctx) => res(ctx.status(200), ctx.json([]))));
+
+      renderWithRouterRedux(<PaperSearchBar />);
+
+      // Type in a search query
+      const searchInput = screen.getByPlaceholderText(/search by/);
+      userEvent.type(searchInput, 'notFound');
+
+      // Once no result comes back from our mocked API, confirm our custom message is rendered
+      await waitFor(() => expect(screen.getByText('No Results Found')).toBeDefined());
     });
   });
 });
