72ec17a2cfda31cd783e277e8a480fda38a0df44
Arad Margalit <aradmar@gmail.com>
2021-01-03 18:00:47 -0800

    Login tests

---
+++ client/src/components/Login/Login.test.tsx
@@ -0,0 +1,23 @@
+import { createLocation } from 'history';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { getBlankInitialState, renderWithRouterRedux } from '../../testUtils/reduxRender';
+import Login from './Login';
+import { blankUser } from '../../templates';
+
+describe('<Login />', () => {
+  describe('without the user having logged in', () => {
+    it('renders the landing page', () => {
+      renderWithRouterRedux(<Login location={createLocation('/')} />);
+      expect(screen.getByText("Read a paper a week. That's it.")).toBeDefined();
+    });
+  });
+
+  describe('with a logged-in user', () => {
+    it('redirects to the dashboard', () => {
+      const initialState = { ...getBlankInitialState(), user: { ...blankUser, display: 'Piranesi' } };
+      renderWithRouterRedux(<Login location={createLocation('/')} />, { redirectTo: '/dashboard', initialState });
+      expect(screen.getByText('Redirected to a new page.')).toBeDefined();
+    });
+  });
+});
