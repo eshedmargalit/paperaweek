b76985761a5e37d4a47d6b2062b350b4c1dde2b4
Eshed Margalit <eshed.margalit@gmail.com>
2021-01-13 21:53:59 -0500

    improvement to modal including latex demo

--- client/src/components/ReviewWizard/HelpModal.tsx
+++ client/src/components/ReviewWizard/HelpModal.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
-import { Modal } from 'antd';
+import { Row, Col, Modal, Button } from 'antd';
 import { ModalFuncProps } from 'antd/lib/modal';
+import LatexDemo from './LatexDemo';
 
 export default function HelpModal({ visible, onCancel, onOk }: ModalFuncProps): JSX.Element {
   return (
     <div>
-      <Modal
-        title="How to Use the Review Form"
-        visible={visible}
-        onCancel={onCancel}
-        onOk={onOk}
-        destroyOnClose
-        width="80%"
-      >
-        <span>So glad you are here!</span>
+      <Modal visible={visible} onCancel={onCancel} onOk={onOk} destroyOnClose footer={null} width="70%">
+        <h2>📝 Write Rich, Detailed Notes</h2>
+        <Row>
+          <Col lg={12} sm={24}>
+            <h5>Full Markdown Support</h5>
+            <p>
+              Enhance your notes with Markdown features like creating lists, italicizing and bolding important content,
+              make tables, and more.
+            </p>
+            <a href="https://devhints.io/markdown" target="_blank" rel="noopener noreferrer">
+              <Button shape="round">Markdown Cheatsheet</Button>
+            </a>
+          </Col>
+          <Col lg={12} sm={24}>
+            <h5>Embedded Math</h5>
+            <p>
+              Use LaTeX to render mathematical equations and scientific symbols. Just wrap your math in dollar signs!
+            </p>
+            <p>
+              To write dollar signs without triggering the math rendering, escape them with slashes: &quot;\$ this will
+              not be math \$&quot;
+            </p>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col lg={24}>
+            <h6>Try it yourself:</h6>
+            <div>
+              <LatexDemo />
+            </div>
+          </Col>
+        </Row>
       </Modal>
     </div>
   );
