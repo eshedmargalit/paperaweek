2562be15748492c7d33a2ce0c3793398f5f24a42
Arad Margalit <aradmar@gmail.com>
2021-01-17 13:30:13 -0800

    another one down!

--- client/src/components/PublicProfile/PublicProfile.test.tsx
+++ client/src/components/PublicProfile/PublicProfile.test.tsx
@@ -7,7 +7,7 @@ import PublicProfile from '.';
 import { getBlankInitialState, renderWithRouterRedux } from '../../testUtils/reduxRender';
 import { Review, User } from '../../types';
 import { PublicProfileMatchParams, PublicProfileReduxProps } from './PublicProfile-redux';
-import { blankUser } from '../../templates';
+import { blankReview, blankUser } from '../../templates';
 import { server } from '../../mocks/server';
 
 // Helper to render the profile with any userId and reviewId
@@ -47,6 +47,35 @@ describe('<PublicProfile />', () => {
         renderWithMatchOptions('12345', 'reviewId', { ...blankUser, googleId: '12345' }, false);
         await waitFor(() => expect(screen.getByText(/Nobody can see/)).toBeInTheDocument());
       });
+    });
+
+    describe('when the profile is public', () => {
+      const userDisplayName = 'The Notorious 321';
+      beforeEach(() => {
+        server.use(
+          rest.get('/api/profiles/321', (_req, res, ctx) => {
+            return res(
+              ctx.status(200),
+              ctx.json({ isOwnPage: true, reviews: [{ ...blankReview, _id: '123' }], userDisplayName })
+            );
+          })
+        );
+      });
+
+      it("shows the user's review", async () => {
+        renderWithMatchOptions('321', 'reviewId', { ...blankUser, googleId: '321' }, true);
+        await waitFor(() => expect(screen.getByText(`${userDisplayName}'s Reviews`)).toBeInTheDocument());
+      });
+    });
+  });
+
+  describe('when the profile does not belong to the logged-in user', () => {
+    describe('when the profile is private', () => {
+      xit('shows the 404 page', () => {});
+    });
+
+    describe('when the profile is public', () => {
+      xit("shows the user's review", () => {});
     });
   });
 });
