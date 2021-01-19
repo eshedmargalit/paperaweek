ffa9b565ea9f300f48b4a6a4c9625eac39bc8600
Arad Margalit <aradmargalit@users.noreply.github.com>
2021-01-19 06:23:14 -0800

    simple maintenance page (#102)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,11 +6,14 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
+import MaintenancePage from './components/MaintenancePage';
+
+// This is set at the highest possible point so we can serve this even if the rest of the server is down
+// and this prevents users from getting around the page by directly hitting endpoints registered by React Router
+const IS_UNDER_MAINTENANCE = false;
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Provider store={store}>{!IS_UNDER_MAINTENANCE ? <App /> : <MaintenancePage />}</Provider>,
   document.getElementById('root')
 );
 
