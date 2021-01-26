da6bff6abd34c3a00ba979b52c8f11a11a61e882
Arad Margalit <aradmar@gmail.com>
2021-01-25 18:42:59 -0800

    fixes some funky behavior, I think

--- client/src/components/Home/Home-redux.tsx
+++ client/src/components/Home/Home-redux.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useProtected } from '../../hooks';
 import HomeContainer from './Home-container';
 
 export default function HomeRedux(): JSX.Element {
-  useProtected();
   return <HomeContainer />;
 }
--- client/src/hooks/useProtected/useProtected.ts
+++ client/src/hooks/useProtected/useProtected.ts
@@ -2,10 +2,8 @@
 // If yes, do nothing.
 // If not, this will redirect the user to login.
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { fetchUser } from '../../actions';
 import { RootState } from '../../reducers';
 import { initialState as initialBlankUser } from '../../reducers/reducer_auth';
 
@@ -17,14 +15,7 @@ const DEFAULT_REDIRECT_PATH = '/';
 
 export function useProtected(options?: UseProtectedOptions): void {
   const { push } = useHistory();
-  const dispatch = useDispatch();
   const { user, loading } = useSelector((state: RootState) => state.auth);
-
-  // by passing [dispatch] as the second argument of useEffect, we replicate the behavior
-  // of componentDidMount + componentDidUnmount, but not componentDidUpdate
-  useEffect(() => {
-    dispatch(fetchUser());
-  }, [dispatch]);
 
   if (!loading && user === initialBlankUser.user) {
     // If this doesn't exist or is equivalent to an empty user, redirect
