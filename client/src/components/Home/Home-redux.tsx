4d453e6dbcfc46c4e171caee652aa8f1b4f8f960
Eshed Margalit <eshed.margalit@gmail.com>
2021-01-26 13:29:28 -0500

    adds spinner for Home while waiting for fetchUser to finish

--- client/src/components/Home/Home-redux.tsx
+++ client/src/components/Home/Home-redux.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Spin } from 'antd';
 import { useProtected } from '../../hooks';
 import HomeContainer from './Home-container';
+import { RootState } from '../../reducers';
+import { AuthState } from '../../reducers/reducer_auth';
 
 export default function HomeRedux(): JSX.Element {
   useProtected();
-  return <HomeContainer />;
+  const { loading }: AuthState = useSelector((state: RootState) => state.auth);
+
+  return (
+    <Spin spinning={loading}>
+      <HomeContainer />
+    </Spin>
+  );
 }
