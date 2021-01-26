13b7309979939f7a2a710ecef56ba3b65be470d0
Eshed Margalit <eshedmargalit@users.noreply.github.com>
2021-01-26 11:36:24 -0500

    adds truncation and prevents overflow for reading list items (#110)

--- client/src/components/ReadingList/ReadingList-view.tsx
+++ client/src/components/ReadingList/ReadingList-view.tsx
@@ -4,78 +4,71 @@ import { Button, Empty, List, PageHeader } from 'antd';
 import { OrderedListOutlined, DeleteOutlined, FormOutlined, MenuOutlined } from '@ant-design/icons';
 import { SortableContainer, SortableElement, SortableHandle, SortEndHandler } from 'react-sortable-hoc';
 import moment from 'moment';
-import Infinite from 'react-infinite';
-import { shortenAuthors } from '../utils';
+import { shortenAuthors, shortenString } from '../utils';
 import './ReadingList.scss';
 import { Paper } from '../../types';
 
 const LIST_HEIGHT = 340;
-const ITEM_HEIGHT = 130;
+const TITLE_CUTOFF = 100;
 
 const DragHandle = SortableHandle(() => (
   <MenuOutlined style={{ fontSize: '14pt', marginBottom: '5px', marginRight: '15px' }} />
 ));
 
 type SortableItemProps = Pick<ReadingListViewProps, 'handleDeleteClick' | 'handleEditClick'> & {
-  height: number;
   value: Paper;
   sortIndex: number;
 };
 
-const SortableItem = SortableElement(
-  ({ height, value, sortIndex, handleEditClick, handleDeleteClick }: SortableItemProps) => (
-    <List.Item style={{ height }}>
-      <div className="reading-list__item">
-        <DragHandle />
-        <div
-          style={{
-            display: 'flex',
-            height: '100%',
-            width: '100%',
-            justifyContent: 'space-between',
-            overflow: 'auto',
-          }}
-        >
-          <div>
-            <List.Item.Meta
-              title={`#${sortIndex + 1}: ${value.title}`}
-              description={`${shortenAuthors(value.authors)}, ${moment(value.date, 'YYYY-MM').format('YYYY')}`}
-            />
+const SortableItem = SortableElement(({ value, sortIndex, handleEditClick, handleDeleteClick }: SortableItemProps) => (
+  <List.Item>
+    <div className="reading-list__item">
+      <DragHandle />
+      <div
+        style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          justifyContent: 'space-between',
+          overflow: 'auto',
+        }}
+      >
+        <div>
+          <List.Item.Meta
+            title={`#${sortIndex + 1}: ${shortenString(value.title, TITLE_CUTOFF)}`}
+            description={`${shortenAuthors(value.authors)}, ${moment(value.date, 'YYYY-MM').format('YYYY')}`}
+          />
+        </div>
+        <div>
+          <div className="reading-list__form-button">
+            <Link to="/form">
+              <Button onClick={() => handleEditClick(value)} icon={<FormOutlined />} />
+            </Link>
           </div>
           <div>
-            <div className="reading-list__form-button">
-              <Link to="/form">
-                <Button onClick={() => handleEditClick(value)} icon={<FormOutlined />} />
-              </Link>
-            </div>
-            <div>
-              <Button onClick={() => handleDeleteClick(value)} icon={<DeleteOutlined />} />
-            </div>
+            <Button onClick={() => handleDeleteClick(value)} icon={<DeleteOutlined />} />
           </div>
         </div>
       </div>
-    </List.Item>
-  )
-);
+    </div>
+  </List.Item>
+));
 
 type SortableInfiniteListProps = Pick<ReadingListViewProps, 'items' | 'handleEditClick' | 'handleDeleteClick'>;
 
 const SortableInfiniteList = SortableContainer(
   ({ items, handleEditClick, handleDeleteClick }: SortableInfiniteListProps) => (
-    <List bordered>
-      <Infinite containerHeight={LIST_HEIGHT} elementHeight={ITEM_HEIGHT}>
-        {items.map((value, index) => (
-          <SortableItem
-            height={ITEM_HEIGHT}
-            key={`item-${value.title}`}
-            index={index}
-            sortIndex={index}
-            value={value}
-            handleEditClick={handleEditClick}
-            handleDeleteClick={handleDeleteClick}
-          />
-        ))}
-      </Infinite>
+    <List bordered style={{ height: LIST_HEIGHT, overflow: 'scroll' }}>
+      {items.map((value, index) => (
+        <SortableItem
+          key={`item-${value.title}`}
+          index={index}
+          sortIndex={index}
+          value={value}
+          handleEditClick={handleEditClick}
+          handleDeleteClick={handleDeleteClick}
+        />
+      ))}
     </List>
   )
 );
@@ -109,7 +102,7 @@ export default function ReadingListView({
     />
   );
   return (
-    <div>
+    <div className="reading-list">
       <br />
       <PageHeader title="Reading List" avatar={{ icon: <OrderedListOutlined /> }} />
       {items.length > 0 ? sortableList : noList}
