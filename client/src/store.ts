8bb404aa07b81df79a78a90e9f0655e53666f60d
Arad Margalit <aradmar@gmail.com>
2021-09-12 01:13:03 -0700

    fixup redux tests

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ConfigureStoreOptions } from '@reduxjs/toolkit';
 import rootReducer from './slices';
 
-const store = configureStore({
+export const storeOptions: ConfigureStoreOptions = {
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-});
+};
+
+const store = configureStore(storeOptions);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
