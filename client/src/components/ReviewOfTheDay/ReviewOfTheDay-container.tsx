998c3728b437d08a2311eeef963fad276a7d9be4
Eshed Margalit <eshed.margalit@gmail.com>
2021-09-13 20:54:10 -0700

    refactor to move container logic into container layer

--- client/src/components/ReviewOfTheDay/ReviewOfTheDay-container.tsx
+++ client/src/components/ReviewOfTheDay/ReviewOfTheDay-container.tsx
@@ -1,8 +1,31 @@
+import { ClockCircleOutlined } from '@ant-design/icons';
+import { PageHeaderProps } from 'antd/lib/page-header';
+import moment from 'moment';
 import React from 'react';
-import ReviewOfTheDayView, { ReviewOfTheDayViewProps } from './ReviewOfTheDay-view';
+import { useMedia } from 'react-media';
+import { Review } from '../../types';
+import ReviewOfTheDayView from './ReviewOfTheDay-view';
 
-type ReviewOfTheDayContainerProps = ReviewOfTheDayViewProps;
+interface ReviewOfTheDayContainerProps {
+  rotd: Review;
+}
 
-export default function ReviewOfTheDayContainer(props: ReviewOfTheDayContainerProps): JSX.Element {
-  return <ReviewOfTheDayView {...props} />;
+export default function ReviewOfTheDayContainer({ rotd }: ReviewOfTheDayContainerProps): JSX.Element {
+  const weeksAgo = (review: Review): number => {
+    return moment().diff(review.createdAt, 'weeks');
+  };
+
+  const title = 'Review Revisit Rewind';
+
+  // Pageheader subtitles look awful on small screens, so we don't bother
+  const isSmallScreen = useMedia({ query: '(max-width: 599px)' });
+  const subTitle = isSmallScreen ? null : `Read ${weeksAgo(rotd)} weeks ago`;
+  const avatar = { icon: <ClockCircleOutlined /> };
+  const pageHeaderProps: PageHeaderProps = {
+    title,
+    subTitle,
+    avatar,
+  };
+
+  return <ReviewOfTheDayView rotd={rotd} pageHeaderProps={pageHeaderProps} />;
 }
--- client/src/components/ReviewOfTheDay/ReviewOfTheDay-view.tsx
+++ client/src/components/ReviewOfTheDay/ReviewOfTheDay-view.tsx
@@ -1,31 +1,20 @@
 import React from 'react';
 
 import { PageHeader } from 'antd';
-import { ClockCircleOutlined } from '@ant-design/icons';
-import moment from 'moment';
-import { useMedia } from 'react-media';
+import { PageHeaderProps } from 'antd/lib/page-header';
 import { Review } from '../../types';
 import ReviewDisplay from '../ReviewDisplay/ReviewDisplay';
 import FadePreview from '../FadePreview/FadePreview';
 
 export interface ReviewOfTheDayViewProps {
   rotd: Review;
+  pageHeaderProps: PageHeaderProps;
 }
 
-const weeksAgo = (review: Review): number => {
-  return moment().diff(review.createdAt, 'weeks');
-};
-
-export default function ReviewOfTheDayView({ rotd }: ReviewOfTheDayViewProps): JSX.Element {
-  const title = 'Review Revisit Rewind';
-
-  // Pageheader subtitles look awful on small screens, so we don't bother
-  const isSmallScreen = useMedia({ query: '(max-width: 599px)' });
-  const subTitle = isSmallScreen ? null : `Read ${weeksAgo(rotd)} weeks ago`;
-
+export default function ReviewOfTheDayView({ rotd, pageHeaderProps }: ReviewOfTheDayViewProps): JSX.Element {
   return (
     <div>
-      <PageHeader title={title} subTitle={subTitle} avatar={{ icon: <ClockCircleOutlined /> }} />
+      <PageHeader {...pageHeaderProps} />
       <FadePreview>
         <ReviewDisplay review={rotd} showTitle />
       </FadePreview>
