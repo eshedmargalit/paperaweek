1d434314eac44261f0435606cbe5288dfbab806b
Eshed Margalit <eshed.margalit@gmail.com>
2021-09-13 20:14:38 -0700

    fixes for smaller screens

--- client/src/components/ReviewOfTheDay/ReviewOfTheDay-view.tsx
+++ client/src/components/ReviewOfTheDay/ReviewOfTheDay-view.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { PageHeader } from 'antd';
 import { ClockCircleOutlined } from '@ant-design/icons';
 import moment from 'moment';
+import { useMedia } from 'react-media';
 import { Review } from '../../types';
 import ReviewDisplay from '../ReviewDisplay/ReviewDisplay';
 import FadePreview from '../FadePreview/FadePreview';
@@ -17,7 +18,11 @@ const weeksAgo = (review: Review): number => {
 
 export default function ReviewOfTheDayView({ rotd }: ReviewOfTheDayViewProps): JSX.Element {
   const title = 'Review Revisit Rewind';
-  const subTitle = `Read ${weeksAgo(rotd)} weeks ago`;
+
+  // Pageheader subtitles look awful on small screens, so we don't bother
+  const isSmallScreen = useMedia({ query: '(max-width: 599px)' });
+  const subTitle = isSmallScreen ? null : `Read ${weeksAgo(rotd)} weeks ago`;
+
   return (
     <div>
       <PageHeader title={title} subTitle={subTitle} avatar={{ icon: <ClockCircleOutlined /> }} />
