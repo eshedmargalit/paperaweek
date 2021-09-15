514ec3f2994c6dfee0eac379cdb66f4e1cefba70
Arad Margalit <aradmar@gmail.com>
2021-09-14 21:59:58 -0700

    fix empty section

--- client/src/components/ReviewOfTheDay/ReviewOfTheDay-redux.tsx
+++ client/src/components/ReviewOfTheDay/ReviewOfTheDay-redux.tsx
@@ -7,20 +7,21 @@ import ReviewOfTheDayContainer from './ReviewOfTheDay-container';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import { hashString } from '../utils';
 
-const MIN_REVIEWS_FOR_ROTD = 5;
-
-const pickROTD = (reviews: Review[]): Review => {
+const pickROTD = (reviews: Review[]): Review | undefined => {
   const dateInt = hashString(moment().format('YYYYMMDD'));
   return reviews[dateInt % reviews.length];
 };
 
 export default function ReviewOfTheDayRedux(): JSX.Element | null {
   const reviewList: Review[] = useAppSelector((state) => state.reviews.reviewList);
-  if (reviewList.length < MIN_REVIEWS_FOR_ROTD) {
-    return null;
-  }
 
   const rotd = pickROTD(reviewList);
+
+  // This could happen if somebody accidentally tries to render this with an empty review list
+  // The home page doesn't show this component if that's the case, so this is just future-proofing
+  if (!rotd) {
+    return null;
+  }
 
   return <ReviewOfTheDayContainer rotd={rotd} />;
 }
