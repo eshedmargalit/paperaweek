3b6745d4e386e826ca14835fc18b61628f5e11a5
Eshed Margalit <eshed.margalit@gmail.com>
2021-11-07 18:53:08 -0800

    factor reading list adder form into separate component

--- client/src/components/ReadingList/ManualReadingListAdder.tsx
+++ client/src/components/ReadingList/ManualReadingListAdder.tsx
@@ -3,20 +3,35 @@ import { Button, Input } from 'antd';
 import React, { useState } from 'react';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import { blankPaper } from '../../templates';
-import { useUpdateReadingListFunc } from './hooks';
+import { useSetReadingList } from './hooks';
+
+function ReadingListAdderForm({ onExit }: { onExit: VoidFunction }): JSX.Element {
+  const [title, setTitle] = useState('');
+
+  const readingList = useAppSelector((state) => state.readingList);
+  const { setReadingList } = useSetReadingList();
+
+  const submitItem = () => {
+    const newReadingList = readingList.concat({ ...blankPaper, title });
+    setReadingList(newReadingList);
+    onExit();
+  };
+
+  return (
+    <div className="reading-list-adder__form">
+      <Input placeholder="Paper title" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <Button aria-label="submit" onClick={submitItem} disabled={title === ''}>
+        <CheckCircleOutlined />
+      </Button>
+      <Button aria-label="cancel" danger onClick={onExit}>
+        <CloseCircleOutlined />
+      </Button>
+    </div>
+  );
+}
 
 export default function ManualReadingListAdder(): JSX.Element {
   const [isEditing, setIsEditing] = useState(false);
-  const [title, setTitle] = useState('');
-  const readingList = useAppSelector((state) => state.readingList);
-
-  const { updateReadingListFunc } = useUpdateReadingListFunc();
-
-  const submitItem = () => {
-    setTitle('');
-    const newReadingList = readingList.concat({ ...blankPaper, title });
-    updateReadingListFunc(newReadingList);
-  };
 
   return (
     <div className="reading-list-adder">
@@ -25,15 +40,7 @@ export default function ManualReadingListAdder(): JSX.Element {
           Add to Reading List <PlusOutlined />
         </Button>
       ) : (
-        <div className="reading-list-adder__form">
-          <Input placeholder="Paper title" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <Button aria-label="submit" onClick={submitItem} disabled={title === ''}>
-            <CheckCircleOutlined />
-          </Button>
-          <Button aria-label="cancel" danger onClick={() => setIsEditing(false)}>
-            <CloseCircleOutlined />
-          </Button>
-        </div>
+        <ReadingListAdderForm onExit={() => setIsEditing(false)} />
       )}
     </div>
   );
