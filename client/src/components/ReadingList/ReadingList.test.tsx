23f73064821a9eea062689e9313a1747d9332e18
Eshed Margalit <eshed.margalit@gmail.com>
2021-11-07 17:49:57 -0800

    clean up tests

--- client/src/components/ReadingList/ReadingList.test.tsx
+++ client/src/components/ReadingList/ReadingList.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import Home from '.';
-import { blankPaper, blankUser, demoUser } from '../../templates';
+import { blankPaper } from '../../templates';
 import { getBlankInitialState, renderWithRouterRedux } from '../../testUtils/reduxRender';
 import { RootState } from '../../store';
 import { ReadingListState } from '../../slices/readingListSlice';
