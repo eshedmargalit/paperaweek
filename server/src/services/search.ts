e120ea19367ad09164e4bfb947b1c6980994604a
Eshed Margalit <eshed.margalit@gmail.com>
2022-01-05 08:07:33 -0800

    adds DOI check before making API request

--- server/src/services/search.ts
+++ server/src/services/search.ts
@@ -2,12 +2,16 @@ import { uniqBy as _uniqBy, flatten as _flatten } from 'lodash';
 import { IPaper } from '../models/Paper';
 import { getPapersByTitle, getPapersByDOI } from '../apis/openalex/openalex';
 
+function isDOI(query: string): boolean {
+  return query.startsWith('10.') || query.includes('doi.org');
+}
+
 async function searchByTitle(query: string): Promise<IPaper[]> {
   return getPapersByTitle(query);
 }
 
 async function searchByDOI(query: string): Promise<IPaper[]> {
-  return getPapersByDOI(query);
+  return isDOI(query) ? getPapersByDOI(query) : [];
 }
 
 export default async function search(query: string): Promise<IPaper[]> {
