edf464f159cc01ef2b14dcc188014268eb16e597
Eshed Margalit <eshed.margalit@gmail.com>
2022-01-07 19:15:52 -0800

    joining the polite queue by adding an email to requests

--- server/src/apis/openalex/openalex.ts
+++ server/src/apis/openalex/openalex.ts
@@ -5,12 +5,13 @@ import { paperFromWork, authorFromAuthorResponse } from './converters';
 import { AuthorsResponse, Work, WorksResponse } from './types';
 
 const OPEN_ALEX_URL = 'https://api.openalex.org';
+const MAILTO = 'paperaweek@protonmail.com';
 const MAX_RESULTS = 5;
 
 export async function getPapersByTitle(title: string): Promise<IPaper[]> {
   try {
     const worksResponse = await axios.get<WorksResponse>(
-      `${OPEN_ALEX_URL}/works?filter=title.search:${title}&per-page=${MAX_RESULTS}`
+      `${OPEN_ALEX_URL}/works?filter=title.search:${title}&per-page=${MAX_RESULTS}&mailto=${MAILTO}`
     );
     const papers: IPaper[] = worksResponse.data.results.map(paperFromWork);
     return papers;
@@ -21,7 +22,7 @@ export async function getPapersByTitle(title: string): Promise<IPaper[]> {
 
 export async function getPapersByDOI(doi: string): Promise<IPaper[]> {
   try {
-    const workResponse = await axios.get<Work>(`${OPEN_ALEX_URL}/works/doi:${doi}`);
+    const workResponse = await axios.get<Work>(`${OPEN_ALEX_URL}/works/doi:${doi}?mailto=${MAILTO}`);
     const paper: IPaper = paperFromWork(workResponse.data);
     return [paper];
   } catch (err) {
@@ -33,7 +34,7 @@ export async function getPapersByDOI(doi: string): Promise<IPaper[]> {
 export async function getAuthorByName(name: string): Promise<Author | null> {
   try {
     const authorsResponse = await axios.get<AuthorsResponse>(
-      `${OPEN_ALEX_URL}/authors?filter=display_name.search:${name}&sort=cited_by_count`
+      `${OPEN_ALEX_URL}/authors?filter=display_name.search:${name}&sort=cited_by_count&mailto=${MAILTO}`
     );
     const author = authorFromAuthorResponse(authorsResponse.data);
     return author;
@@ -46,7 +47,7 @@ export async function getAuthorByName(name: string): Promise<Author | null> {
 export async function getPapersByAuthor(author: Author): Promise<IPaper[]> {
   try {
     const worksResponse = await axios.get<WorksResponse>(
-      `${OPEN_ALEX_URL}/works?filter=author.id:${author.id}&per-page=${MAX_RESULTS}`
+      `${OPEN_ALEX_URL}/works?filter=author.id:${author.id}&per-page=${MAX_RESULTS}&mailto=${MAILTO}`
     );
     const papers: IPaper[] = worksResponse.data.results.map(paperFromWork);
     return papers;
